a36cb86aa72d8ce9fb572c7ebf5ff4881d5864a1
JimChe <75532532+JimChe@users.noreply.github.com>
2022-01-03 21:56:22 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,23 +1,38 @@
 import os
 
 from linebot import LineBotApi, WebhookParser
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, TemplateSendMessage, ButtonsTemplate
 
 
-channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", "4Vmuh1fBve0HDaj1/1BsxCdedVtwV5TMgY6KVQUt4g8V75GwftAt8NzC3PHT+xULn5BADYODpMj7XD5dP9nd603dfGtir8egUAJCJnG+jBxMx1Jiw7sClpiif/wZT8b3pdXnv+JbWKGqOsDq2vFM3AdB04t89/1O/w1cDnyilFU=")
 
+line_bot_api = LineBotApi(channel_access_token)
 
 def send_text_message(reply_token, text):
-    line_bot_api = LineBotApi(channel_access_token)
     line_bot_api.reply_message(reply_token, TextSendMessage(text=text))
 
     return "OK"
 
 
-"""
-def send_image_url(id, img_url):
-    pass
+def send_image_url(reply_token, img_url):
+    message = ImageSendMessage(
+        original_content_url = img_url,
+        preview_image_url = img_url
+    )
+    line_bot_api.reply_message(reply_token, message)
+    
+    return "OK"
 
-def send_button_message(id, text, buttons):
-    pass
-"""
+def send_button_message(reply_token, title, text, btn, url):
+    message = TemplateSendMessage(
+        alt_text='button template',
+        template = ButtonsTemplate(
+            title = title,
+            text = text,
+            thumbnail_image_url = url,
+            actions = btn
+        )
+    )
+    line_bot_api.reply_message(reply_token, message)
+
+    return "OK"
