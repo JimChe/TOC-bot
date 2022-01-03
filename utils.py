a8bd2e7c62a1f52fe50fffea313ceb0188ee7143
JimChe <75532532+JimChe@users.noreply.github.com>
2022-01-03 23:42:50 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import os
 
 from linebot import LineBotApi, WebhookParser
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage, TemplateSendMessage, ButtonsTemplate
+from linebot.models import MessageEvent, \
+TextMessage, TextSendMessage, ImageSendMessage, \
+TemplateSendMessage, ButtonsTemplate, ImageCarouselTemplate
 
 
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", "4Vmuh1fBve0HDaj1/1BsxCdedVtwV5TMgY6KVQUt4g8V75GwftAt8NzC3PHT+xULn5BADYODpMj7XD5dP9nd603dfGtir8egUAJCJnG+jBxMx1Jiw7sClpiif/wZT8b3pdXnv+JbWKGqOsDq2vFM3AdB04t89/1O/w1cDnyilFU=")
@@ -32,6 +34,16 @@ def send_button_message(reply_token, title, text, btn, url):
             thumbnail_image_url = url,
             actions = btn
         )
+    )
+    line_bot_api.reply_message(reply_token, message)
+
+    return "OK"
+
+
+def send_carousel_message(reply_token, col):
+    message = TemplateSendMessage(
+        alt_text = 'Carousel template',
+        template = ImageCarouselTemplate(columns = col)
     )
     line_bot_api.reply_message(reply_token, message)
 
